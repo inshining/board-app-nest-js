64cd9c184fbfd1fbe619a744274484024f1c48ab
inshining <inshining.kim@gmail.com>
2022-10-27 22:34:42 +0900

    ✏️[CORRECT] : tap size 조정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 
 @Module({
-  imports: [BoardsModule],
-  controllers: [],
-  providers: [],
+    imports: [BoardsModule],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
